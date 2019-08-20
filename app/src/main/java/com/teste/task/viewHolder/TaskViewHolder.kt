bf7834f0e2fc7db23f69f8737080fcf5b48e46f9
Diego1906 <di_fsantos@yahoo.com.br>
2019-08-19 23:26:29 -0300

    Adição dos icones que serão usados na aplicação. Ajuste na view para começar a fazer a bind dos dados com os elementos do layout. Implementação de elementos no layout do item da task.

--- app/src/main/java/com/teste/task/viewHolder/TaskViewHolder.kt
+++ app/src/main/java/com/teste/task/viewHolder/TaskViewHolder.kt
@@ -2,6 +2,7 @@ package com.teste.task.viewHolder
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import com.teste.task.R
 import com.teste.task.entities.TaskEntity
@@ -9,8 +10,24 @@ import com.teste.task.entities.TaskEntity
 class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private val mTextDescription: TextView = itemView.findViewById(R.id.textDescription)
+    private val mTextPriority: TextView = itemView.findViewById(R.id.textPriority)
+    private val mTextDueDate: TextView = itemView.findViewById(R.id.textDueDate)
+    private val mImageTask: ImageView = itemView.findViewById(R.id.imageTask)
 
     fun bindData(taskEntity: TaskEntity) {
         mTextDescription.text = taskEntity.description
+        mTextPriority.text = taskEntity.priorityId.toString()
+        mTextDueDate.text = taskEntity.duedate
+
+        var resId: Int = 0
+        when (taskEntity.complete) {
+            true -> {
+                resId = R.drawable.ic_done
+            }
+            else -> {
+                resId = R.drawable.ic_todo
+            }
+        }
+        mImageTask.setImageResource(resId)
     }
 }
